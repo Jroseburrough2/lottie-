7f0df39d0db8c5c3037adf87e1f58480a2081f0c
Jroseburrough2 <84330984+Jroseburrough2@users.noreply.github.com>
2021-05-20 15:18:29 -0500

    Create lottie.js

---
+++ lottie.js
@@ -0,0 +1,2 @@
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets10.lottiefiles.com/datafiles/wW9k6ALg5Mn9cIj/data.json"  background="transparent"  speed="1"  style="width: 600px; height: 600px;"  loop  autoplay></lottie-player>
